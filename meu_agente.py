e241404cf7abdd3058f6fd167b2755f3f7ed7725
Felipe <fstizzianidias@gmail.com>
2025-11-01 15:21:21 -0300

    Corrigindo para usar create_react_agent do LangGraph

--- meu_agente.py
+++ meu_agente.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 from calendar_tool import criar_calendario_tool, listar_calendarios_tool, listar_eventos_calendario_tool, criar_evento_programado_tool, excluir_evento_tool, atualizar_evento_tool
 from datetime import datetime, timedelta, timezone
-from langchain.agents import create_agent
+from langgraph.prebuilt import create_react_agent
 from langchain_core.messages import HumanMessage
 import sqlite3
 from langgraph.checkpoint.sqlite import SqliteSaver
@@ -80,7 +80,7 @@ memory = SqliteSaver(conexao)
 google_calendar_tools = [criar_calendario_tool, listar_calendarios_tool, listar_eventos_calendario_tool,
                          criar_evento_programado_tool, excluir_evento_tool, atualizar_evento_tool]
 
-agente_google_calendar = create_agent(
+agente_google_calendar = create_react_agent(
     model=llm,
     tools=google_calendar_tools,
     system_prompt=prompt_sys,
